58c09d0bc6bd6d080aedba8e2cc9340615a88334
pbruyninckx <pbruyninckx@gmail.com>
2020-12-01 20:34:16 +0000

    Day 1 part 2

--- src/aoc/day01.clj
+++ src/aoc/day01.clj
@@ -12,6 +12,22 @@
   (let [[a b] (find-pair #{} numbers)]
     (* a b)))
 
+(defn subsets [[el & remaining :as s] n]
+  (cond
+    (= n 0) '(())
+    (< (count s) n) '()
+    :else
+    (lazy-cat
+      (map #(conj % el) (subsets remaining (dec n)))
+      (subsets remaining n))))
+
+(defn solve2 [numbers]
+  (let [triplets (subsets numbers 3)
+        [solution-triplet] (filter #(= 2020 (reduce + %)) triplets)]
+    (reduce * solution-triplet)))
+
 (defn day1 []
   (with-open [rdr (io/reader "resources/input01.txt")]
-    (println (solve (map #(Integer/parseInt %) (line-seq rdr))))))
+    (let [numbers (map #(Integer/parseInt %) (line-seq rdr))]
+      (println (solve numbers))
+      (println (solve2 numbers)))))
