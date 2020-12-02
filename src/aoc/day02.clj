ad98bbf5a9f02dfcaef081dbcd800c8c3b8d62f4
pbruyninckx <pbruyninckx@gmail.com>
2020-12-02 20:06:27 +0000

    day 2 passwords

--- src/aoc/day02.clj
+++ src/aoc/day02.clj
@@ -1,4 +1,35 @@
-(ns aoc.day02)
+(ns aoc.day02
+  (:require [clojure.java.io :as io]))
+
+(def line-re
+  #"(?<min>\d+)-(?<max>\d+) (?<char>.): (?<code>\w+)" )
+
+(defn parse-line [line]
+  (let [matcher (re-matcher line-re line)]
+    (.matches matcher)
+    {:min  (Integer/parseInt (.group matcher "min"))
+     :max  (Integer/parseInt (.group matcher "max"))
+     :char (first (.group matcher "char"))
+     :code (.group matcher "code")
+     }))
+
+(defn valid-password? [{:keys [min max char code]}]
+  (<= min
+      (count (filter #{char} code))
+      max))
+
+(defn valid-password2? [{:keys [min max char code]}]
+  (not= (= char (get code (dec min)))
+        (= char (get code (dec max)))))
+
+(defn solve [passwords]
+  (count (filter valid-password? passwords)))
+
+(defn solve2 [passwords]
+  (count (filter valid-password2? passwords)))
 
 (defn run []
-  (println "day2"))
+  (with-open [rdr (io/reader "resources/input02.txt")]
+    (let [passwords (map parse-line (line-seq rdr))]
+      (println (solve passwords))
+      (println (solve2 passwords)))))
