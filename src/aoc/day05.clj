c1b8cddc6f2af6932981ce9fa08ac1cbcb2915c5
pbruyninckx <pbruyninckx@gmail.com>
2020-12-05 12:09:15 +0000

    Day 5 part two

--- src/aoc/day05.clj
+++ src/aoc/day05.clj
@@ -1,5 +1,6 @@
 (ns aoc.day05
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io])
+  (:require [clojure.set :as set]))
 
 (defn seat-id [boarding-pass]
   (reduce
@@ -12,7 +13,14 @@
 (defn solve [boarding-passes]
   (reduce max (map seat-id boarding-passes)))
 
+(defn solve2 [boarding-passes]
+  (let [ids (set (map seat-id boarding-passes))]
+    (first (filter
+             #(and (not (ids %)) (ids (inc %)) (ids (dec %)))
+             (range)))))
+
 (defn run []
   (with-open [rdr (io/reader "resources/input05.txt")]
     (let [lines (line-seq rdr)]
-      (println (solve lines)))))
+      (println (solve lines))
+      (println (solve2 lines)))))
