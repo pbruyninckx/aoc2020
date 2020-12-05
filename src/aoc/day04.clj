23ff12947b9b0b40eeb80eacc5a0eedb32e779b5
pbruyninckx <pbruyninckx@gmail.com>
2020-12-05 00:12:16 +0000

    Day 4 part 2
    That started to feel a little bit tedious.

--- src/aoc/day04.clj
+++ src/aoc/day04.clj
@@ -34,12 +34,38 @@
               "pid"}
             (set (keys passport)))))
 
+(defn is-number-and-between? [s d low high]
+  (and (re-matches (case d 2 #"\d{2}" 3 #"\d{3}" 4 #"\d{4}") s)
+       (let [n (Integer/parseInt s)]
+         (<= low n high))))
+
+(defn valid-passport-fields? [passport]
+  (and (valid-passport? passport)
+       (is-number-and-between? (passport "byr") 4 1920 2002)
+       (is-number-and-between? (passport "iyr") 4 2010 2020)
+       (is-number-and-between? (passport "eyr") 4 2020 2030)
+       (let [full-height (passport "hgt")
+             number-str (str/join (drop-last 2 full-height))
+             unit (take-last 2 full-height)]
+         (cond (= unit [\c \m])
+               (is-number-and-between? number-str 3 150 193)
+               (= unit [\i \n])
+               (is-number-and-between? number-str 2 59 76)
+               :else
+               false))
+       (re-matches #"#[0-9a-f]{6}" (passport "hcl"))
+       (#{"amb" "blu" "brn" "gry" "grn" "hzl" "oth"} (passport "ecl"))
+       (re-matches #"\d{9}" (passport "pid"))))
+
 (defn solve [passports]
   (count (filter valid-passport? passports)))
+
+(defn solve2 [passports]
+  (count (filter valid-passport-fields? passports)))
 
 (defn run []
   (with-open [rdr (io/reader "resources/input04.txt")]
     (let [lines (line-seq rdr)
           passports (read-passports lines)]
-      (println (first passports))
-      (println (solve passports)))))
+      (println (solve passports))
+      (println (solve2 passports)))))
