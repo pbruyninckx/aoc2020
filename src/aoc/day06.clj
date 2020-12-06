cbc1089dbdd5b1b770eea5d321a8ece3087a2d4e
pbruyninckx <pbruyninckx@gmail.com>
2020-12-06 13:05:21 +0000

    Day 6 part 2
    This was a very small change.
    Long live functions as first class objects.

--- src/aoc/day06.clj
+++ src/aoc/day06.clj
@@ -2,16 +2,18 @@
   (:require [clojure.java.io :as io])
   (:require [clojure.set :as set]))
 
-(defn solve [lines]
+(defn solve [lines set-op]
   (->> lines
        (partition-by empty?)
        (take-nth 2)
        (map #(map set %))
-       (map #(apply set/union %))
+       (map #(apply set-op %))
        (map count)
        (reduce +)))
 
 (defn run []
   (with-open [rdr (io/reader "resources/input06.txt")]
     (let [lines (line-seq rdr)]
-      (println (solve lines)))))
+      (doall (map
+               #(println (solve lines %))
+               [set/union set/intersection])))))
