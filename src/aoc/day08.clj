d0cbcefeb5f91ba27eaa4fb420230605f42963f4
pbruyninckx <pbruyninckx@gmail.com>
2020-12-08 10:00:22 +0000

    Day 8 part 2

--- src/aoc/day08.clj
+++ src/aoc/day08.clj
@@ -24,19 +24,48 @@
       "jmp"
       (update console :line #(+ % arg)))))
 
+(defn terminated? [^console {:keys [instructions line]}]
+  (= line (count instructions)))
+
 (defn new-console [program]
   (->console program 0 0))
 
-(defn solve [program]
+(defn run-program [program]
   (loop [console (new-console program)
          executed #{}]
-    (if (executed (:line console))
-      (:acc console)
+    (if (or (executed (:line console))
+            (terminated? console))
+      console
       (recur (step console)
              (conj executed (:line console))))))
+
+(defn solve [program]
+  (:acc (run-program program)))
+
+(defn changed-programs [program]
+  "The problem is small enough to brute force"
+  (letfn [(can-switch [^instruction instruction]
+            (#{"jmp" "nop"} (:op instruction)))
+          (switch-arg [^instruction instruction]
+            (case (:op instruction)
+              "jmp" (assoc instruction :op "nop")
+              "nop" (assoc instruction :op "jmp")))]
+    (map #(update program % switch-arg)
+         (filter #(can-switch (program %))
+                 (range (count program))))))
+
+
+(defn solve2 [program]
+  (let [maybe-fixed-programs (changed-programs program)]
+    (loop [[p & remaining] maybe-fixed-programs]
+      (let [end-state (run-program p)]
+        (if (terminated? end-state)
+          (:acc end-state)
+          (recur remaining))))))
 
 (defn run []
   (with-open [rdr (io/reader "resources/input08.txt")]
     (let [lines (line-seq rdr)
           program (parse-program lines)]
-      (println (solve program)))))
+      (println (solve program))
+      (println (solve2 program)))))
