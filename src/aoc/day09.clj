6eb854ae111e6257b81bf03befb20338143fa9ce
pbruyninckx <pbruyninckx@gmail.com>
2020-12-09 23:07:38 +0000

    Day 9 part 2
    A little bit too fidgetty, but it seems to work.

--- src/aoc/day09.clj
+++ src/aoc/day09.clj
@@ -20,8 +20,21 @@
        first
        last))
 
+(defn solve2 [numbers target]
+  (let [cumsum (vec (reductions + numbers))]
+    (filter identity
+            (for [max-ind (range 1 (count numbers))
+                  min-ind (range 0 (count numbers))]
+              (if (and (= target (- (cumsum max-ind) (nth cumsum (dec min-ind) 0)))
+                       (not= min-ind max-ind))
+                (let [solution-range (->> numbers (drop min-ind) (take (inc (- max-ind min-ind))))]
+                  (+ (apply min solution-range) (apply max solution-range)))
+                nil)))))
+
 (defn run[]
   (with-open [rdr (io/reader "resources/input09.txt")]
     (let [lines (line-seq rdr)
-          numbers (map #(Long/parseLong %) lines)]
-      (println (solve numbers)))))
+          numbers (map #(Long/parseLong %) lines)
+          solution1 (solve numbers)]
+      (println solution1)
+      (println (solve2 numbers solution1)))))
