010f2be5579669cc6a7b968176c0039f252dfc20
pbruyninckx <pbruyninckx@gmail.com>
2020-12-12 13:46:33 +0000

    Day 12 part two

--- src/aoc/day12.clj
+++ src/aoc/day12.clj
@@ -17,10 +17,30 @@
     \L (update state 2 #((apply comp (repeat (/ n 90) rotate-left)) %))
     \R (update state 2 #((apply comp (repeat (/ n 90) rotate-right)) %))))
 
+(defn rotate-delta-left [[dx dy]]
+  [(- dy) dx])
+(defn rotate-delta-right [[dx dy]]
+  [dy (- dx)])
+
+(defn next-state2 [[[x y] [dx dy :as delta] :as state] [action n]]
+  (case action
+    \N (update-in state [1 1] #(+ % n))
+    \S (update-in state [1 1] #(- % n))
+    \W (update-in state [1 0] #(- % n))
+    \E (update-in state [1 0] #(+ % n))
+    \F (update state 0 #(mapv (fn [p dp] (+ p (* n dp))) % delta))
+    \L (update state 1 #((apply comp (repeat (/ n 90) rotate-delta-left)) %))
+    \R (update state 1 #((apply comp (repeat (/ n 90) rotate-delta-right)) %))))
+
 (defn solve [directions]
   (let [start-state [0 0 \E]
         [x y _] (reduce next-state start-state directions)]
     (reduce + (map #(Math/abs ^int %) [x y]))))
+
+(defn solve2 [directions]
+  (let [start-state [[0 0] [10 1]]
+        [pos & _] (reduce next-state2 start-state directions)]
+    (reduce + (map #(Math/abs ^int %) pos))))
 
 (defn read-data []
   (let [input-lines (->> (slurp "resources/input12.txt")
@@ -32,4 +52,5 @@
 
 (defn run []
   (let [directions (read-data)]
-    (println (solve directions))))
+    (println (solve directions))
+    (println (solve2 directions))))
