505d4c8620d25250382b7835a173eeb5f1ba9e84
pbruyninckx <pbruyninckx@gmail.com>
2020-12-13 13:07:29 +0000

    Day 13 part two
    It was convenient that all the bus numbers happened to be primes.

--- src/aoc/day13.clj
+++ src/aoc/day13.clj
@@ -7,6 +7,18 @@
              (apply min-key #(- % (rem start-time %))))]
     (* number (- number (rem start-time number)))))
 
+(defn add-bus [[start-time step-size] [ind n]]
+  (->> (range start-time (+ start-time (* step-size n)) step-size)
+       (filter #(zero? (rem (+ % ind) n)))
+       first
+       (#(vector % (* step-size n)))))
+
+(defn solve2 [indexed-bus-numbers]
+  (first
+    (reduce add-bus
+            (first indexed-bus-numbers)
+            (rest indexed-bus-numbers))))
+
 (defn read-data[]
   (let [[start-time-str bus-nbr-str] (str/split-lines (slurp "resources/input13.txt"))
         bus-nbrs (->> (str/split bus-nbr-str #",")
@@ -17,5 +29,9 @@
 
 (defn run[]
   (let [[start-time all-bus-numbers] (read-data)
-        bus-numbers (filter pos-int? all-bus-numbers)]
-    (println (solve start-time bus-numbers))))
+        bus-numbers (filter pos-int? all-bus-numbers)
+        indexed-bus (->> all-bus-numbers
+                         (map-indexed vector)
+                         (filter #(pos-int? (second %))))]
+    (println (solve start-time bus-numbers))
+    (println (solve2 indexed-bus))))
