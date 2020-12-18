5fbe720e2eefed44138f1ba2f7b0b91611c524d9
pbruyninckx <pbruyninckx@gmail.com>
2020-12-18 23:05:03 +0000

    Day 16 part two
    This took a little bit longer than expected.
    Partly caused by me reinstalling everything on my computer,
    and partly for being mentally frightened by the searching algorithm.
    Though it looks like it was easier than expected,
    and it didn't even needed any backtracking (although the code should be
    able to do so).

--- src/aoc/day16.clj
+++ src/aoc/day16.clj
@@ -29,7 +29,53 @@
          (filter (complement valid-for-any-constraint))
          (reduce +))))
 
+(defn remove-invalid-tickets [{:keys [nearby constraints] :as data}]
+  (letfn [(invalid-for-all-constraints [n]
+            (not-any? #(% n) constraints))]
+    (->> nearby
+         (filter (complement #(some invalid-for-all-constraints %)))
+         (vec)
+         (assoc data :nearby))))
+
+(defn get-col [n nearby]
+  (mapv #(get % n) nearby))
+
+(defn get-constraint-matrix [{:keys [constraints nearby]}]
+  "each row is a constraint, each column applies to certain field values"
+  (mapv vec
+        (partition (count constraints)
+                   (for [constraint constraints
+                         column (map #(get-col % nearby) (range (count (nearby 0))))]
+                     (every? constraint column)))))
+
+(defn print-mat [mat]
+  (doall
+    (map #(println (apply str %))
+         (partition 20 (map {false \. true \#}
+                            mat)))))
+
+(defn resolve-constraints [mat correspondence n]
+  (cond
+    (= n (count correspondence))
+    correspondence
+    :else
+    (->> (range (count correspondence))
+         (filter #(not-any? #{%} correspondence))
+         (filter #(get-in mat [n %]))
+         (sort-by #(count(filter identity (get-col % (drop n mat)))))
+         (map #(resolve-constraints mat (assoc correspondence n %) (inc n)))
+         first)))
+
+(defn solve2 [data]
+  (let [constraint-matrix (get-constraint-matrix data)]
+    (let [mappings (resolve-constraints constraint-matrix
+                         (vec (repeat (count constraint-matrix) nil))
+                         0)]
+      (reduce * (map #((:mine data) %) (take 6 mappings))))))
+
 (defn run []
   (let [input-string (slurp "resources/input16.txt")
-        data (parse-data input-string)]
-    (println (solve data))))
+        data (parse-data input-string)
+        valid-data (remove-invalid-tickets data)]
+    (println (solve data))
+    (println (solve2 valid-data))))
