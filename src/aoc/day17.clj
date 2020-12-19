0211eb051604a7186c36710a05f7b016b5d64e78
pbruyninckx <pbruyninckx@gmail.com>
2020-12-19 19:04:18 +0000

    Day 17 part one

---
+++ src/aoc/day17.clj
@@ -0,0 +1,60 @@
+(ns aoc.day17
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
+
+(defn flatten-1 [l]
+  (mapcat seq l))
+
+(defn parse-data [s]
+  (let [indexed-data
+        (map-indexed
+          (fn [row-index row]
+            (map-indexed (fn [col-index char]
+                           ({\# [row-index col-index 0]} char))
+                         row))
+          (str/split-lines s))]
+    (->> indexed-data
+         flatten-1
+         (filter identity)
+         set)))
+
+(defn neighbours [[x y z]]                                  ; xy- switch doesn't matter here
+  (letfn [(neighbours-1 [el] (range (dec el) (+ 2 el)))]
+    (for [xn (neighbours-1 x)
+          yn (neighbours-1 y)
+          zn (neighbours-1 z)
+          :when (not (and (= x xn) (= y yn) (= z zn)))]
+      [xn yn zn])))
+
+(defn next-cycle [data]
+  (let [current-neighbours
+        (set/difference (->> data
+                             (map neighbours)
+                             flatten-1
+                             set)
+                        data)]
+    (set/union
+      (->> data
+           (filter #(<= 2
+                        (count (set/intersection
+                                 (set (neighbours %))
+                                 data))
+                        3))
+           set)
+      (->> current-neighbours
+           (filter #(= 3
+                       (count (set/intersection
+                                (set (neighbours %))
+                                data))))
+           set))))
+
+(defn solve [data]
+  (loop [data data n 0]
+    (println n)
+    (if (= n 6)
+      (count data)
+      (recur (next-cycle data) (inc n)))))
+
+(defn run[]
+  (let [data (parse-data (slurp "resources/input17.txt"))]
+    (println (solve data))))
