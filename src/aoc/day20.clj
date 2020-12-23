177ba3ef0f6e8e5af613827a454cb95827e3c8f4
pbruyninckx <pbruyninckx@gmail.com>
2020-12-23 18:47:54 +0000

    Day 20 part one
    Let's cross the fingers to hope part two doesn't need
    the solving of the whole puzzle.

---
+++ src/aoc/day20.clj
@@ -0,0 +1,54 @@
+(ns aoc.day20
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
+
+(defn parse-tile [[header & tile]]
+  [;Get tile number
+   (->> header
+        (re-find #"\d+")
+        (Integer/parseInt))
+   ;Get actual tile as vector of vectors for fast access
+   (mapv vec tile)])
+
+(defn parse-data [s]
+  (->> s
+       str/split-lines
+       (partition-by empty?)
+       (take-nth 2)
+       (map parse-tile)
+       (into {})))
+
+(defn get-borders [tile direction]
+  (let [borders
+        (mapv #(% tile)
+        [first last #(mapv first %) #(mapv last %)])
+        reverse-borders
+        (map #(vec (reverse %)) borders)]
+    (case direction
+      :both (into borders reverse-borders)
+      :orig borders)))
+
+(defn solve [data]
+  (let [edge-borders
+        (->> data
+             vals
+             (map #(get-borders % :both))
+             (mapcat seq)
+             frequencies
+             (filter #(= 1  (% 1)))
+             (map #(% 0))
+             set)]
+    (letfn [(is-edge-tile [[_ tile]]
+              (->> (get-borders tile :orig)
+                   set
+                   (set/intersection edge-borders)
+                   count
+                   (= 2)))]
+      (->> data
+           (filter is-edge-tile)
+           (map #(% 0))
+           (apply *)))))
+
+(defn run []
+  (let [data (parse-data (slurp "resources/input20.txt"))]
+    (println (solve data))))
